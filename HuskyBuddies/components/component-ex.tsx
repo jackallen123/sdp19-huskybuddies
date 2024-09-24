0e589b8165759ea6c2b9ff3398dffd409971b604
Jack Allen <jackallen1100@gmail.com>
2024-09-24 15:42:02 -0400

    initial project setup

---
+++ HuskyBuddies/components/component-ex.tsx
@@ -0,0 +1,3 @@
+/*
+../components is where you can put reusable components that aren't individual ui components (like button)
+*/
---
+++ HuskyBuddies/components/ui/button.tsx
@@ -0,0 +1,6 @@
+/* 
+../components/ui is where individual ui components will go
+
+This will help us to be consistent across different pages. If you need a UI component for your page,
+check here first to see if we already have it. If not, create it and add it here.
+*/
