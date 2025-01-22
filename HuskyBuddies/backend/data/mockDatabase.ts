c43eeef1a25c673893643c2979b441cabfc04345
Jack Allen <jackallen1100@gmail.com>
2025-01-22 11:49:27 -0500

    added interfaces for home page, addCourse, addSection, and schedule

--- HuskyBuddies/backend/data/mockDatabase.ts
+++ HuskyBuddies/backend/data/mockDatabase.ts
@@ -9,18 +9,44 @@ export interface User {
   password: string;
 }
 
-export const mockUsers: User[] = [
-  {
-    id: "1",
-    firstName: "Admin",
-    lastName: "User",
-    email: "admin@uconn.edu",
-    password: "admin",
-  },
-];
+// event-related types and data
+export interface Event {
+  id: number;
+  name: string;
+  date: string;
+  location: string;
+}
 
-// mock data for login and signup screens
-export const mockLoginData = {
-  email: "admin@uconn.edu",
-  password: "admin",
-};
+// study Buddy related types and data
+export interface StudyBuddy {
+  id: number;
+  name: string;
+  sharedClasses: number;
+  profilePicture: string;
+}
+
+// resource-related types and data
+export interface ResourceItem {
+  name: string;
+  url: string;
+}
+
+// course-related types and data
+export interface Course {
+  id: string;
+  code: string;
+  name: string;
+  instructor: string;
+  section: string;
+  color: string;
+  startTime: string;
+  endTime: string;
+  days: string[];
+}
+
+// section-related types and data
+export interface Section {
+  sectionNumber: string;
+  meets: string;
+  instructor: string;
+}
