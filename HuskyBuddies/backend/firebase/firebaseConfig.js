4d7069202f3787c8d260a495aacd44417d0ae350
Jack Allen <jackallen1100@gmail.com>
2025-01-30 17:30:00 -0500

    added .env.example and completed firebaseConfig

--- HuskyBuddies/backend/firebase/firebaseConfig.js
+++ HuskyBuddies/backend/firebase/firebaseConfig.js
@@ -1,14 +1,15 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import Constants from "expo-constants";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "sdp19-huskybuddies-b9250.firebaseapp.com",
-  projectId: "sdp19-huskybuddies-b9250",
-  storageBucket: "sdp19-huskybuddies-b9250.appspot.com",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: Constants.expoConfig?.extra?.FIREBASE_API_KEY,
+  authDomain: Constants.expoConfig?.extra?.FIREBASE_AUTH_DOMAIN,
+  projectId: Constants.expoConfig?.extra?.FIREBASE_PROJECT_ID,
+  storageBucket: Constants.expoConfig?.extra?.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: Constants.expoConfig?.extra?.FIREBASE_MESSAGING_SENDER_ID,
+  appId: Constants.expoConfig?.extra?.FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
