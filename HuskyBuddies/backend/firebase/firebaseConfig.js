4743773d5cdb3e0187a5fba890fcaf7d216bcf07
Jack Allen <jackallen1100@gmail.com>
2025-01-30 17:04:11 -0500

    installed firebase package, initialized config file, defined android package name and ios bundle identifier in app.json

---
+++ HuskyBuddies/backend/firebase/firebaseConfig.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "sdp19-huskybuddies-b9250.firebaseapp.com",
+  projectId: "sdp19-huskybuddies-b9250",
+  storageBucket: "sdp19-huskybuddies-b9250.appspot.com",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export { auth, db };
